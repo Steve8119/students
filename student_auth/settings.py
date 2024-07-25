348b798225be270e1e4f5d57107064e2d85c6546
Steve8119 <stevenwaithaka8119@gmail.com>
2024-07-25 12:08:54 +0300

    updated  ALLOWED_HOSTS

--- student_auth/settings.py
+++ student_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b)x+rnr1i&zx!!d+feq2dpa=izww!u=!&9_3gy#cr(#c+=g6%i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
