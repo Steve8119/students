d4c48ca3d3be228fbdbb8faf579bcc4d554396ac
Steve8119 <stevenwaithaka8119@gmail.com>
2024-07-25 21:56:40 +0300

    updated urls.py

--- student_auth/urls.py
+++ student_auth/urls.py
@@ -1,9 +1,11 @@
+# student_auth/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls')),
-    path('parents/', include('parents.urls')),
-    path('administration/', include('administration.urls')),
+    path('', include('authentication.urls')),  # Ensure this matches your app's URLs
+    path('parents/', include('parents.urls')),  # Ensure this matches your app's URLs
+    path('administration/', include('administration.urls')),  # Ensure this matches your app's URLs
 ]
